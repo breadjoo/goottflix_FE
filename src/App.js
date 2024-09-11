f5b85e89710690ac4765882afcef23e582957f06
breadjoo <yoohwanjoo@nate.com>
2024-09-11 12:36:54 +0900

    [Feat,Chore] React 기본설정, 메인페이지 기본컨셉 제작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import ImageSlider from "./components/ImageSlider";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Navbar />
+          <ImageSlider />
+          <Header />
+        <div className="container">
+          <MainContent />
+        </div>
+        <Footer />
+      </div>
   );
 }
 
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import '../css/ImageSlider.css'; // CSS 파일을 추가
+
+const images = [
+    '/images/에일리언.jpg',
+    '/images/비틀쥬스.jpg',
+    '/images/인사이드아웃2.jpg',
+];
+
+const ImageSlider = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000); // 5초마다 이미지 변경
+
+        return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 제거
+    }, []);
+
+    return (
+        <div
+            className="image-slider"
+            style={{
+                backgroundImage: `url(${images[currentImageIndex]})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '70vh', // 높이를 화면의 50%로 설정 (반으로 줄임)
+                position: 'relative', // 텍스트를 배경 위에 오버레이
+            }}
+        >
+            <div className="overlay"></div>
+            <div className="slider-content">
+                <h1>당신에게 최고의 영화는 무엇인가요?</h1>
+                <p>구트구트 플릭쯔 ~.</p>
+                <button className="cta-button">button입니다!</button>
+            </div>
+        </div>
+    );
+};
+
+
+export default ImageSlider;
